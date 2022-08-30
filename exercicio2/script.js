c0c3a65bd0561ec33e76b7e2be8a2654d3660b47
layanemnzswok@gmail.com <layanemnzswork@gmail.com>
2022-08-30 17:04:25 -0300

    Exercícios Operadores a008

---
+++ exercicio2/script.js
@@ -0,0 +1,20 @@
+//Pergunte ao usuário dois números
+
+const numero1 = prompt("Digite aqui o seu primeiro número")
+
+const numero2 = prompt("Digite aqui o seu segundo número")
+
+//O primeiro número é maior que segundo? true
+console.log(numero1 > numero2)
+
+
+//O primeiro número é igual que segundo? false
+console.log(numero1 === numero2)
+
+
+//O primeiro número é divisível pelo segundo? true
+console.log(numero1 % numero2 === 0)
+
+
+//O segundo número é divisível pelo primeiro? true
+console.log(numero2 % numero1 === 0)
